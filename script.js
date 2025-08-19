46857b8d6554bd1df9d7f742d71c09669c3eb176
Ankit <136968673+AnkitKumarDelta@users.noreply.github.com>
2025-08-19 10:59:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ function convertToRoman(num) {
 	let ones = ["","I","II","III","IV","V","VI","VII","VIII","IX"];
 	let tens = ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"];
 	let hund = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"];
-    let thou = ["","M","MM","MMM"];
-	return thou[Math.floor(num/1000)] 
+    
+	let thou = "";
+  let thousandsCount = Math.floor(num / 1000);
+  for (let i = 0; i < thousandsCount; i++) {
+    thou += "M";
+  }
+	return thou 
        + hund[Math.floor((num%1000)/100)] 
        + tens[Math.floor((num%100)/10)] 
        + ones[num%10];
